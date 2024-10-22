a65080f75bf5942642e5879ae904a8548485b341
Maxence Choiselle <mchoiselle@gmail.com>
2024-10-22 13:57:18 +0200

    [Feat] : Ex 3 completed =) !

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,12 +1,35 @@
 
 function PokemonCard (){
+  const pokemon = pokemonList[1]
+ 
+ 
   return (
-  <figure>
+    
 
-  <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="image de bulbizare" />
-  <figcaption>Bulbazaur</figcaption>
-  </figure>)
+  <figure>
+    {pokemon.imgSrc ? (<img src={pokemon.imgSrc} alt="image du pokemon" />)
+: 
+(<p>????</p>)
 }
+
+  <figcaption>{pokemon.name}</figcaption>
+
+  </figure>
+  )
+
+  
+}
+
+const pokemonList = [
+  {
+    name: "Bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "Mew",
+  },
+];
 
 
 
