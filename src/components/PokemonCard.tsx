13c8a147593ff43f9d6a615c8d249068f9e594a0
Maxence Choiselle <mchoiselle@gmail.com>
2024-10-22 11:38:58 +0200

    [Feat] : add bulazaur on branch components.1

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,17 @@
+
+function PokemonCard (){
+  return (
+  <figure>
+
+  <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="image de bulbizare" />
+  <figcaption>Bulbazaur</figcaption>
+  </figure>)
+}
+
+
+
+
+
+
+
+export default PokemonCard;
